1982a1ed4a44e0f24eba4b5d8aef1cddc61bd615
tuber <btu@thoughtworks.com>
2015-08-21 20:23:14 +0800

    [tuber] update TeacherTest.java

--- src/main/java/com/thoughtworks/btu/Teacher.java
+++ src/main/java/com/thoughtworks/btu/Teacher.java
@@ -1,33 +1,26 @@
 package com.thoughtworks.btu;
 
-import com.thoughtworks.btu.rule.BuzzRule;
-import com.thoughtworks.btu.rule.BuzzWhizzRule;
-import com.thoughtworks.btu.rule.FizzBuzzRule;
-import com.thoughtworks.btu.rule.FizzBuzzWhizzRule;
-import com.thoughtworks.btu.rule.FizzDigitalRule;
-import com.thoughtworks.btu.rule.FizzRule;
-import com.thoughtworks.btu.rule.FizzWhizzRule;
-import com.thoughtworks.btu.rule.Rule;
-import com.thoughtworks.btu.rule.WhizzRule;
-
-import java.util.ArrayList;
-import java.util.List;
-
 public class Teacher {
-    private static List<Rule> rules;
-    public static void build(Integer fizzNumber, Integer buzzNumber, Integer whizzNumber) {
-        rules = new ArrayList<>();
-        rules.add(new FizzDigitalRule(fizzNumber));
-        rules.add(new FizzBuzzWhizzRule(fizzNumber, buzzNumber, whizzNumber));
-        rules.add(new FizzBuzzRule(fizzNumber, buzzNumber));
-        rules.add(new FizzWhizzRule(fizzNumber, whizzNumber));
-        rules.add(new BuzzWhizzRule(buzzNumber, whizzNumber));
-        rules.add(new FizzRule(fizzNumber));
-        rules.add(new BuzzRule(buzzNumber));
-        rules.add(new WhizzRule(whizzNumber));
+    private Integer fizzNumber;
+    private Integer buzzNumber;
+    private Integer whizzNumber;
+
+    public Integer getFizzNumber() {
+        return fizzNumber;
     }
 
-    public static List<Rule> getRules() {
-        return rules;
+    public Integer getBuzzNumber() {
+        return buzzNumber;
+    }
+
+    public Integer getWhizzNumber() {
+        return whizzNumber;
+    }
+
+    public boolean say(Integer fizzNumber, Integer buzzNumber, Integer whizzNumber) {
+        this.fizzNumber = fizzNumber;
+        this.buzzNumber = buzzNumber;
+        this.whizzNumber = whizzNumber;
+        return true;
     }
 }
--- src/test/java/com/thoughtworks/btu/TeacherTest.java
+++ src/test/java/com/thoughtworks/btu/TeacherTest.java
@@ -6,8 +6,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class TeacherTest {
     @Test
-    public void should_generate_rules_if_teacher_has_the_three_special_number() {
-        Teacher.build(3, 5, 7);
-        assertThat(Teacher.getRules()).isNotNull();
+    public void should_return_true_if_teacher_say_three_special_number_success() {
+        Teacher teacher = new Teacher();
+        assertThat(teacher.say(3, 5, 7)).isTrue();
+
     }
 }
