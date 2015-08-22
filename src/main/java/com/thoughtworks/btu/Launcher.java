511c57985eb1e51d8ff686359e129e8d40e16f04
tuber <btu@thoughtworks.com>
2015-08-22 11:07:26 +0800

    [tuber] update Launcher class

--- src/main/java/com/thoughtworks/btu/Launcher.java
+++ src/main/java/com/thoughtworks/btu/Launcher.java
@@ -1,18 +1,31 @@
 package com.thoughtworks.btu;
 
+import com.thoughtworks.btu.rule.Rule;
+import com.thoughtworks.btu.rule.RuleFactory;
+
+import java.util.List;
+import java.util.Scanner;
+
 public class Launcher {
 
-    public static final int START = 1;
-    public static final int END = 100;
+    public static final int COUNT = 100;
 
-//    public static void main(String[] asdf) {
-//        Scanner scanner = new Scanner(System.in);
-//        while (scanner.hasNext()) {
-//            Teacher.build(scanner.nextInt(), scanner.nextInt(), scanner.nextInt());
-//            Student student = new Student();
-//            for (int i = START; i <= END; ++i) {
-//                System.out.println(student.answer(i));
-//            }
-//        }
-//    }
+    public static void main(String[] asdf) {
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNext()) {
+            Teacher teacher = new Teacher();
+            teacher.say(scanner.nextInt(), scanner.nextInt(), scanner.nextInt());
+            List<Rule> rules = RuleFactory.build(teacher);
+            for (int i = 0; i < COUNT; ++i) {
+                Integer location = i + 1;
+                Student student = new Student(location);
+                for (Rule rule : rules) {
+                    if (student.isConformBy(rule)) {
+                        System.out.println(student.say());
+                        break;
+                    }
+                }
+            }
+        }
+    }
 }
